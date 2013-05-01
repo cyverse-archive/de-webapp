cyverse-archive/de-webapp

commit 1dbc71c6f0ea31c999c991385c6d23a8d52deb33

core-3944 fix issues with out of date path being submitted to jex when there is a folder rename

src/main/java/org/iplantc/de/client/models/ClientDataModel.java
@@ -241,6 +241,15 @@ public class ClientDataModel {
         }
     }
 
+    private void updateFilePath(String pathNew) {
+        for (DiskResource dr : page.getResources()) {
+            if (dr instanceof File) {
+                ((File)dr).setPath(pathNew);
+                dr.setId(pathNew + "/" + dr.getName());
+            }
+        }
+    }
+
     public Folder getFolder(final String path) {
         return heirarchy.findModel("id", path); //$NON-NLS-1$
     }
@@ -255,7 +264,8 @@ public class ClientDataModel {
             ret.setName(DiskResourceUtil.parseNameFromPath(pathNew));
 
             heirarchy.update(ret);
-
+            page.setPath(pathNew);
+            updateFilePath(pathNew);
             updateSubtreeIds(subfolders, pathOrig, pathNew);
         }
 

commit 8b8b2dc37a09038c27fa0e60e2bc413c0bc15c96

CORE-4258 Fixed folder rename logic.
Fixed folder rename bug in ClientDataModel (introduced in 1dbc71c6f0ea31c999c991385c6d23a8d52deb33 for CORE-3944).
Updated rename logic in DataMainPanel (added in commit dbf453e533ff5100758ffaed28cd8cf320fb2fbe for CORE-2798) so that it's compatible with these fixes.

src/main/java/org/iplantc/de/client/models/ClientDataModel.java
@@ -241,7 +241,7 @@ public class ClientDataModel {
         }
     }
 
-    private void updateFilePath(String pathNew) {
+    private void updateFilePaths(String pathNew) {
         for (DiskResource dr : page.getResources()) {
             if (dr instanceof File) {
                 ((File)dr).setPath(pathNew);
@@ -264,8 +264,12 @@ public class ClientDataModel {
             ret.setName(DiskResourceUtil.parseNameFromPath(pathNew));
 
             heirarchy.update(ret);
-            page.setPath(pathNew);
-            updateFilePath(pathNew);
+
+            // Check if the currently viewed folder was renamed.
+            if (isCurrentPage(pathOrig)) {
+                page.setPath(pathNew);
+            }
+            updateFilePaths(pathNew);
             updateSubtreeIds(subfolders, pathOrig, pathNew);
         }
 

src/main/java/org/iplantc/de/client/views/panels/DataMainPanel.java
@@ -343,50 +343,31 @@ public class DataMainPanel extends AbstractDataPanel implements DataContainer {
         }
     }
 
-    private void updatePath(final String pathOrig, final String path) {
-        if (model == null) {
+    public void rename(final String pathOrig, final String pathNew) {
+        if (grid == null) {
             return;
         }
 
-        if (model.isCurrentPage(pathOrig)) {
+        if (model != null && model.isCurrentPage(pathOrig)) {
             // The currently viewed folder was renamed.
-            model.getPage().setPath(path);
-
             // Update the IDs of all disk resources loaded in the grid's store.
             ListStore<DiskResource> store = grid.getStore();
             for (DiskResource resource : store.getModels()) {
-                resource.setId(path + "/" + DiskResourceUtil.parseNameFromPath(resource.getId())); //$NON-NLS-1$
+                resource.setId(pathNew + "/" + DiskResourceUtil.parseNameFromPath(resource.getId())); //$NON-NLS-1$
             }
-        } else if (model.isCurrentPage(DiskResourceUtil.parseParent(path))) {
-            // A folder inside the currently viewed folder was renamed.
+        } else {
+            // A resource inside the currently viewed folder was renamed.
             DiskResource resource = findDiskResource(pathOrig);
 
             if (resource != null) {
-                resource.setId(path);
-                resource.setName(DiskResourceUtil.parseNameFromPath(path));
-
-                grid.getStore().update(resource);
-            }
-        }
-    }
-
-    public void rename(final String nameOrig, final String nameNew) {
-        if (grid != null) {
-            DiskResource resource = findDiskResource(nameOrig);
-
-            if (resource != null) {
                 // drop the path from our name
-                resource.setName(DiskResourceUtil.parseNameFromPath(nameNew));
+                resource.setName(DiskResourceUtil.parseNameFromPath(pathNew));
 
                 // our id is the fully qualified path and name
-                resource.setId(nameNew);
+                resource.setId(pathNew);
 
                 grid.getStore().update(resource);
-                if (resource instanceof Folder) {
-                    updatePath(nameOrig, nameNew);
-                }
             }
-
         }
     }
 