dc99b59beb732c800ce1498fba2c4035123f138a
Sriram Srinivasan <sriram@iplantcollaborative.org>
2013-05-07 15:35:26 -0700

    core-3415 add preferences help in GXT3

--- src/main/java/org/iplantc/de/client/preferences/views/PreferencesDialog.java
+++ src/main/java/org/iplantc/de/client/preferences/views/PreferencesDialog.java
@@ -4,15 +4,21 @@
 package org.iplantc.de.client.preferences.views;
 
 import org.iplantc.core.uicommons.client.events.EventBus;
+import org.iplantc.core.uicommons.client.views.gxt3.dialogs.IPlantDialog;
+import org.iplantc.core.uicommons.client.widgets.ContextualHelpPopup;
 import org.iplantc.de.client.I18N;
 import org.iplantc.de.client.events.PreferencesUpdatedEvent;
 import org.iplantc.de.client.preferences.presenter.PreferencesPresenterImpl;
 import org.iplantc.de.client.preferences.views.PreferencesView.Presenter;
 
-import com.sencha.gxt.widget.core.client.Dialog;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.user.client.ui.HTML;
+import com.sencha.gxt.core.client.XTemplates;
 import com.sencha.gxt.widget.core.client.button.ButtonBar;
 import com.sencha.gxt.widget.core.client.button.TextButton;
-import com.sencha.gxt.widget.core.client.button.ToolButton;
+import com.sencha.gxt.widget.core.client.container.AbstractHtmlLayoutContainer.HtmlData;
+import com.sencha.gxt.widget.core.client.container.HtmlLayoutContainer;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
 
@@ -23,17 +29,21 @@ import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
  * @author sriram
  * 
  */
-public class PreferencesDialog extends Dialog {
+public class PreferencesDialog extends IPlantDialog {
 
     private Presenter presenter;
-    private ToolButton help_tool;
+
+    public interface HtmlLayoutContainerTemplate extends XTemplates {
+        @XTemplate(source = "PreferencesHelpTemplate.html")
+        SafeHtml getTemplate();
+    }
 
     public PreferencesDialog() {
+        super(true);
         setHeadingText(I18N.DISPLAY.preferences());
-        help_tool = new ToolButton(ToolButton.QUESTION);
-        getHeader().addTool(help_tool);
         setPixelSize(450, 380);
         setButtons();
+        addHelp();
         PreferencesView view = new PreferencesViewImpl();
         presenter = new PreferencesPresenterImpl(view);
         presenter.go(this);
@@ -44,7 +54,32 @@ public class PreferencesDialog extends Dialog {
         buttonBar.clear();
         setDefaultsButton();
         setOkButton();
+    }
 
+    private void addHelp() {
+        gelHelpToolButton().addSelectHandler(new SelectHandler() {
+
+            @Override
+            public void onSelect(SelectEvent event) {
+                ContextualHelpPopup popup = new ContextualHelpPopup();
+                popup.add(contructHelpView());
+                popup.showAt(help_tool.getAbsoluteLeft(), help_tool.getAbsoluteTop() + 15);
+            }
+        });
+    }
+
+    private HtmlLayoutContainer contructHelpView() {
+        HtmlLayoutContainerTemplate templates = GWT.create(HtmlLayoutContainerTemplate.class);
+        HtmlLayoutContainer c = new HtmlLayoutContainer(templates.getTemplate());
+        c.add(new HTML(I18N.DISPLAY.notifyemail()), new HtmlData(".emailHeader"));
+        c.add(new HTML(I18N.HELP.notifyemailHelp()), new HtmlData(".emailHelp"));
+        c.add(new HTML(I18N.DISPLAY.rememberFileSectorPath()), new HtmlData(".filePathHeader"));
+        c.add(new HTML(I18N.HELP.rememberFileSectorPathHelp()), new HtmlData(".filePathHelp"));
+        c.add(new HTML(I18N.DISPLAY.saveSession()), new HtmlData(".saveSessionHeader"));
+        c.add(new HTML(I18N.HELP.saveSessionHelp()), new HtmlData(".saveSessionHelp"));
+        c.add(new HTML(I18N.DISPLAY.defaultOutputFolder()), new HtmlData(".defaultOp"));
+        c.add(new HTML(I18N.HELP.defaultOutputFolderHelp()), new HtmlData(".defaultOpHelp"));
+        return c;
     }
 
     private void setOkButton() {
