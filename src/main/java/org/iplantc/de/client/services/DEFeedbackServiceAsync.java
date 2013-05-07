cbe735ec148c0d0979efc846080846a93a907b64
Dennis Roberts <dennis@iplantcollaborative.org>
2013-05-06 19:14:31 -0700

    CORE-4264: created a service facade for the DE feedback service

--- src/main/java/org/iplantc/de/client/services/DEFeedbackServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package org.iplantc.de.client.services;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface DEFeedbackServiceAsync {
-
-    void submitFeedback(String feedback, AsyncCallback<String> callback);
-
-}
