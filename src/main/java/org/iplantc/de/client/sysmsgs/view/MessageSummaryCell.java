13acd7518bf18047c8ce29e6007b8eccf3a3c688
Tony Edgin <tedgin@iplantcollaborative.org>
2013-05-29 15:49:08 -0700

    IN PROGRESS - issue CORE-4296: The DE needs to have a window for viewing system messages. https://pods.iplantcollaborative.org/jira/browse/CORE-4296
    The activation date is displayed as today for dates on the current date
    and as the weekday for the days within the previous week.

--- src/main/java/org/iplantc/de/client/sysmsgs/view/MessageSummaryCell.java
+++ src/main/java/org/iplantc/de/client/sysmsgs/view/MessageSummaryCell.java
@@ -1,5 +1,7 @@
 package org.iplantc.de.client.sysmsgs.view;
 
+import java.util.Date;
+
 import org.iplantc.de.client.sysmsgs.model.Message;
 import org.iplantc.de.client.sysmsgs.view.DefaultMessagesViewResources.Style;
 
@@ -8,6 +10,7 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.BrowserEvents;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.Event;
@@ -15,42 +18,37 @@ import com.sencha.gxt.cell.core.client.AbstractEventCell;
 import com.sencha.gxt.core.client.XTemplates;
 import com.sencha.gxt.widget.core.client.event.XEvent;
 
-
 /**
  * This is the cell used to render message summaries.
  */
 final class MessageSummaryCell extends AbstractEventCell<Message> {
 	
-    /**
-     * The XTemplates interface for rendering the HTML.
-     */
 	interface Templates extends XTemplates {
-
-        /**
-         * Creates the HTML.
-         * 
-         * @param msg the message displayed
-         * @param style the style used
-         * @return the HTML
-         */
         @XTemplate(source = "MessageSummary.html")
-        SafeHtml make(Message msg, Style style);
+        SafeHtml make(Message msg, String activationTimeMsg, Style style);
 	}
 	
 	private static final Style CSS;
     private static final Templates FACTORY;
+    private static final long DAYS_PER_WEEK;
+    private static final long MS_PER_DAY;
 
     static {
         FACTORY = GWT.create(Templates.class);
         CSS = GWT.<DefaultMessagesViewResources> create(DefaultMessagesViewResources.class).style();
     	CSS.ensureInjected();
+        DAYS_PER_WEEK = 7;
+        MS_PER_DAY = 86400000;
     }
  
+    private final DateTimeFormat activationTimeFormat;
+
     /**
      * the constructor
      */
     MessageSummaryCell() {
         super(BrowserEvents.CLICK);
+        activationTimeFormat = DateTimeFormat.getFormat("dd MMMM yyyy");
 	}
 
     /**
@@ -74,7 +72,33 @@ final class MessageSummaryCell extends AbstractEventCell<Message> {
 	@Override
 	public void render(final Context context, final Message message, 
 			final SafeHtmlBuilder builder) {
-        builder.append(FACTORY.make(message, CSS));
-	}	
+	    final Date actTime = message.getActivationTime();
+        final Date now = new Date();
+        String actMsg = "";
+        if (sameDay(now, actTime)) {
+            actMsg = "Today";
+        } else if (withinPreviousWeek(now, actTime)) {
+            actMsg = DateTimeFormat.getFormat("cccc").format(actTime);
+        } else {
+            actMsg = activationTimeFormat.format(actTime);
+        }
+        builder.append(FACTORY.make(message, actMsg, CSS));
+    }
 	
+    private boolean sameDay(final Date lhs, final Date rhs) {
+        return activationTimeFormat.format(lhs).equals(activationTimeFormat.format(rhs));
+    }
+
+    private boolean withinPreviousWeek(final Date successor, final Date predecessor) {
+        if (predecessor.after(successor)) {
+            return false;
+        }
+        final Date succDay = activationTimeFormat.parseStrict(activationTimeFormat.format(successor));
+        final Date predDay = activationTimeFormat.parseStrict(activationTimeFormat.format(predecessor));
+        if (predDay.getTime() + (DAYS_PER_WEEK - 1) * MS_PER_DAY <= succDay.getTime()) {
+            return false;
+        }
+        return true;
+    }
+
 }
