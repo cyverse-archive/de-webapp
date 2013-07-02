d532bd72fc8dd12fa854829d7ceeee7a847a40e3
Sriram Srinivasan <sriram@iplantcollaborative.org>
2013-07-02 09:37:24 -0700

    core-4505 fix idrop lite height width

--- src/main/java/org/iplantc/de/client/views/windows/IDropLiteAppletWindow.java
+++ src/main/java/org/iplantc/de/client/views/windows/IDropLiteAppletWindow.java
@@ -29,7 +29,7 @@ public class IDropLiteAppletWindow extends IplantWindowBase {
     public IDropLiteAppletWindow(IDropLiteWindowConfig config) {
         super("");
         this.idlwc = config;
-        setSize("720", "430");
+        setSize("850", "430");
         setResizable(false);
         init();
     }
