7b29beb0ee7fef5d2bd9d86f9c9058eddcbf9994
Paul Sarando <psarando@iplantcollaborative.org>
2013-07-03 17:48:47 -0700

    CORE-4539 Allow Share Notifications to be clicked.
    Added a getPaths method to PayloadData AutoBeans for Sharing Notifications.
    Updated the context processing logic in the NotificationCallback::onSuccess method to check for "share" actions in Data notifications, and to create the appropriate DiskResource context from the first path found in those notifications.

--- src/main/java/org/iplantc/de/client/notifications/models/payload/PayloadData.java
+++ src/main/java/org/iplantc/de/client/notifications/models/payload/PayloadData.java
@@ -1,5 +1,7 @@
 package org.iplantc.de.client.notifications.models.payload;
 
+import java.util.List;
+
 import org.iplantc.core.uicommons.client.models.diskresources.File;
 
 import com.google.web.bindery.autobean.shared.AutoBean.PropertyName;
@@ -19,5 +21,17 @@ public interface PayloadData {
     @PropertyName("action")
     String getAction();
 
+    /**
+     * Present only for "file_uploaded" actions.
+     * 
+     * @return File object
+     */
     File getData();
+
+    /**
+     * Present only for "share" actions.
+     * 
+     * @return A list of paths that have been shared.
+     */
+    List<String> getPaths();
 }
