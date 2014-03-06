fb7d0e89bc69ea85d7983827377de42283d44604
Paul Sarando <psarando@iplantcollaborative.org>
2014-03-05 18:15:06 -0700

    Updated About to include extra build info in dev.
    Updated the AboutApplicationServlet to include additional information from the MANIFEST.MF file in its JSON response (originally removed in commit 7be88b6e70d31d8c98d86fbd7532f2eeef043caa).
    Updated AboutApplicationData to include this extra info.
    Refactored the AboutApplicationWindow's contents from a string template into AboutApplicationAppearance interfaces: an AboutApplicationDefaultAppearance for production and an AboutApplicationDevAppearance for dev environments.
    Removed the explicit module name from the gwt compile section in the pom.xml, so that it can be specified by Jenkins or from the command-line (with -Dgwt.module=org.iplantc.de.discoveryenvironment or -Dgwt.module=org.iplantc.de.discoveryenvironmentDev).

--- src/main/java/org/iplantc/de/client/views/windows/AboutApplicationWindow.java
+++ src/main/java/org/iplantc/de/client/views/windows/AboutApplicationWindow.java
@@ -8,6 +8,7 @@ import org.iplantc.de.client.views.windows.configs.AboutWindowConfig;
 import org.iplantc.de.client.views.windows.configs.ConfigFactory;
 import org.iplantc.de.commons.client.ErrorHandler;
 import org.iplantc.de.resources.client.IplantResources;
+import org.iplantc.de.resources.client.messages.I18N;
 import org.iplantc.de.shared.services.AboutApplicationServiceFacade;
 
 import com.google.gwt.core.client.GWT;
@@ -17,7 +18,6 @@ import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
 import com.google.web.bindery.autobean.shared.AutoBeanCodex;
 
-import com.sencha.gxt.core.client.util.Format;
 import com.sencha.gxt.widget.core.client.ContentPanel;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 
@@ -27,16 +27,18 @@ import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
  * @author lenards
  */
 public class AboutApplicationWindow extends IplantWindowBase {
+
     private AboutApplicationData model;
     private final DeResources res;
+    private final AboutApplicationAppearance appearance;
 
     public AboutApplicationWindow(AboutWindowConfig config) {
         super("");
         setSize("320", "260");
-        setResizable(false);
-        setTitle(org.iplantc.de.resources.client.messages.I18N.DISPLAY.aboutDiscoveryEnvironment());
+        setTitle(I18N.DISPLAY.aboutDiscoveryEnvironment());
         executeServiceCall();
         res = GWT.create(DeResources.class);
+        appearance = GWT.create(AboutApplicationAppearance.class);
     }
 
     private void executeServiceCall() {
@@ -77,16 +79,10 @@ public class AboutApplicationWindow extends IplantWindowBase {
         ContentPanel pnlDetails = new ContentPanel();
         pnlDetails.setHeaderVisible(false);
 
-        HTML txt = new HTML(Format.substitute(getAboutTemplate(), model.getReleaseVersion(),
-                model.getBuildNumber(), Window.Navigator.getUserAgent(), 
-                org.iplantc.de.resources.client.messages.I18N.DISPLAY.projectCopyrightStatement().asString(), org.iplantc.de.resources.client.messages.I18N.DISPLAY.nsfProjectText().asString()));
-        pnlDetails.add(txt);
+        pnlDetails.add(new HTML(appearance.about(model, Window.Navigator.getUserAgent(),
+                I18N.DISPLAY.projectCopyrightStatement(), I18N.DISPLAY.nsfProjectText())));
 
         return pnlDetails;
-    }
-
-    private String getAboutTemplate() {
-        return "<p style='font-style:italic;'> {4} </p><p>Release: {0}</p><p> Build #: {1}</p><p>User Agent: {2}</p> <p style='font-weight:700'> {3}</p>"; //$NON-NLS-1$
     }
 
     @Override
--- src/main/java/org/iplantc/de/server/AboutApplicationServlet.java
+++ src/main/java/org/iplantc/de/server/AboutApplicationServlet.java
@@ -9,6 +9,9 @@ import net.sf.json.JSONObject;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 import javax.servlet.ServletException;
 
 /**
@@ -24,6 +27,12 @@ public class AboutApplicationServlet extends RemoteServiceServlet implements Abo
 
     private static final long serialVersionUID          = 6046105023536377635L;
     private static final String DEFAULT_RELEASE_VERSION = "unversioned";
+    private static final String MANIFEST_LOC            = "/META-INF/MANIFEST.MF";
+    private static final String BUILD_NUMBER_ATTR       = "Jenkins-Build-Number";
+    private static final String BUILD_ID_ATTR           = "Jenkins-Build-ID";
+    private static final String BUILD_COMMIT_ATTR       = "Git-Commit";
+    private static final String BUILD_BRANCH_ATTR       = "Git-Branch";
+    private static final String BUILD_JDK_ATTR          = "Build-Jdk";
 
     /**
      * The logger for error and informational messages.
@@ -34,6 +43,7 @@ public class AboutApplicationServlet extends RemoteServiceServlet implements Abo
      * The DE configuration properties.
      */
     private DiscoveryEnvironmentProperties deProps;
+    private Attributes manifestAttrs;
 
     /**
      * The default constructor.
@@ -75,30 +85,26 @@ public class AboutApplicationServlet extends RemoteServiceServlet implements Abo
      * @return a JSON string containing the build number and release version.
      */
     private String produceInfo() {
-        String buildNumber = getBuildNumber();
-        String relVersion = getReleaseVersion();
-        String response = buildResponse(buildNumber, relVersion);
+        JSONObject json = new JSONObject();
+
+        json.put("release", getReleaseVersion());
+        json.put("build", getBuildName());
+        json.put("buildNumber", getBuildNumber());
+        json.put("buildId", getManifestAttribute(BUILD_ID_ATTR));
+        json.put("buildCommit", getManifestAttribute(BUILD_COMMIT_ATTR));
+        json.put("buildBranch", getManifestAttribute(BUILD_BRANCH_ATTR));
+        json.put("buildJdk", getManifestAttribute(BUILD_JDK_ATTR));
+
+        String response = json.toString(4);
         LOG.debug("the about application JSON is: " + response);
         return response;
     }
 
     /**
-     * Builds the response body.
-     *
-     * @param buildNumber the build number to include in the response.
-     * @param relVersion the release version number to include in the response.
-     * @return the response body.
-     */
-    private String buildResponse(String buildNumber, String relVersion) {
-        JSONObject json = new JSONObject();
-        json.put("buildnumber", buildNumber);
-        json.put("release", relVersion);
-        return json.toString(4);
-    }
-
-    /**
-     * Get the release version that is stored in the application properties file.
-     *
+     * Get the release version to report in the about application screen. If the release version is
+     * available in the DiscoveryEnvironmentProperties, then that release version will be used.
+     * Otherwise, the default release version will be used.
+     * 
      * @return a string representation of the release version.
      */
     private String getReleaseVersion() {
@@ -107,13 +113,53 @@ public class AboutApplicationServlet extends RemoteServiceServlet implements Abo
     }
 
     /**
-     * Get the build number to report in the about application screen. If the build number is available in the project
-     * manifest then that build number will be used. Otherwise, the default build number will be obtained from the
-     * properties file.
-     *
+     * Get the build name to report in the about application screen. If the build name is available in
+     * the DiscoveryEnvironmentProperties, then that build name will be used. Otherwise, the build number
+     * will be used.
+     * 
+     * @return a string representation of the build name.
+     */
+    private String getBuildName() {
+        String buildName = deProps.getDefaultBuildNumber();
+        return StringUtils.isNotEmpty(buildName) ? buildName : getBuildNumber();
+    }
+
+    /**
+     * Get the build number from the project manifest.
+     * 
      * @return a string representation of the build number.
      */
     private String getBuildNumber() {
-        return deProps.getDefaultBuildNumber();
+        return getManifestAttribute(BUILD_NUMBER_ATTR);
+    }
+
+    /**
+     * Attempts to load the main {@link Attributes} from the manifest file.
+     */
+    private void getManifestAttributes() {
+        try {
+            Manifest manifest = new Manifest(getServletContext().getResourceAsStream(MANIFEST_LOC));
+            manifestAttrs = manifest.getMainAttributes();
+        } catch (Exception e) {
+            LOG.error("unable to get Manifest Attributes", e);
+        }
+    }
+
+    /**
+     * Attempts to obtain an attribute's value from the manifest file.
+     * 
+     * @return the attribute's value or an empty value (null or the empty string) if the manifest or its
+     *         attribute is not available.
+     */
+    private String getManifestAttribute(String attr) {
+        if (manifestAttrs == null) {
+            getManifestAttributes();
+        }
+
+        if (manifestAttrs != null) {
+            return manifestAttrs.getValue(attr);
+        }
+
+        return null;
     }
 }
